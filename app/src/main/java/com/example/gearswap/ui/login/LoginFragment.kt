56513c809848ef20969ca6baf221e78bc61f105e
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-04-23 16:01:24 -0400

    Update Login Screen: Resize Buttons
    - Updates the login screen layout with full size buttons
    - All elements are moved higher up the screen to avoid overlap with keyboard
    - Browse as Guess button now navigates to homescreen when clicked

--- app/src/main/java/com/example/gearswap/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/gearswap/ui/login/LoginFragment.kt
@@ -1,9 +1,5 @@
 package com.example.gearswap.ui.login
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.annotation.StringRes
-import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -11,15 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.gearswap.databinding.FragmentLoginBinding
-
 import com.example.gearswap.R
+import com.example.gearswap.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
@@ -28,68 +23,59 @@ class LoginFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
 
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
+        loginViewModel =
+            ViewModelProvider(this, LoginViewModelFactory()).get(LoginViewModel::class.java)
 
         val usernameEditText = binding.username
         val passwordEditText = binding.password
         val loginButton = binding.login
         val loadingProgressBar = binding.loading
 
-        loginViewModel.loginFormState.observe(viewLifecycleOwner,
-            Observer { loginFormState ->
-                if (loginFormState == null) {
-                    return@Observer
-                }
-                loginButton.isEnabled = loginFormState.isDataValid
-                loginFormState.usernameError?.let {
-                    usernameEditText.error = getString(it)
-                }
-                loginFormState.passwordError?.let {
-                    passwordEditText.error = getString(it)
-                }
-            })
+        loginViewModel.loginFormState.observe(viewLifecycleOwner, Observer { loginFormState ->
+            if (loginFormState == null) {
+                return@Observer
+            }
+            loginButton.isEnabled = loginFormState.isDataValid
+            loginFormState.usernameError?.let { usernameEditText.error = getString(it) }
+            loginFormState.passwordError?.let { passwordEditText.error = getString(it) }
+        })
 
-        loginViewModel.loginResult.observe(viewLifecycleOwner,
-            Observer { loginResult ->
-                loginResult ?: return@Observer
-                loadingProgressBar.visibility = View.GONE
-                loginResult.error?.let {
-                    showLoginFailed(it)
-                }
-                loginResult.success?.let {
-                    updateUiWithUser(it)
-                }
-            })
+        loginViewModel.loginResult.observe(viewLifecycleOwner, Observer { loginResult ->
+            loginResult ?: return@Observer
+            loadingProgressBar.visibility = View.GONE
+            loginResult.error?.let { showLoginFailed(it) }
+            loginResult.success?.let { updateUiWithUser(it) }
+        })
 
         val afterTextChangedListener = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int, after: Int
+            ) {
                 // ignore
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int, count: Int
+            ) {
                 // ignore
             }
 
             override fun afterTextChanged(s: Editable) {
                 loginViewModel.loginDataChanged(
-                    usernameEditText.text.toString(),
-                    passwordEditText.text.toString()
+                    usernameEditText.text.toString(), passwordEditText.text.toString()
                 )
             }
         }
@@ -98,8 +84,7 @@ class LoginFragment : Fragment() {
         passwordEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 loginViewModel.login(
-                    usernameEditText.text.toString(),
-                    passwordEditText.text.toString()
+                    usernameEditText.text.toString(), passwordEditText.text.toString()
                 )
             }
             false
@@ -107,11 +92,10 @@ class LoginFragment : Fragment() {
 
         loginButton.setOnClickListener {
             loadingProgressBar.visibility = View.VISIBLE
-            loginViewModel.login(
-                usernameEditText.text.toString(),
-                passwordEditText.text.toString()
-            )
+            loginViewModel.login(usernameEditText.text.toString(), passwordEditText.text.toString())
         }
+
+        binding.guestLogin.setOnClickListener { findNavController().navigate(R.id.navigation_home) }
     }
 
     private fun updateUiWithUser(model: LoggedInUserView) {
