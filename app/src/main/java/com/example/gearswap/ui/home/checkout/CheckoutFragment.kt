1a5716c2f2544e29d67231d3620e213ceff54233
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-04-27 04:48:22 -0600

    Add stripe payment to checkout

--- app/src/main/java/com/example/gearswap/ui/home/checkout/CheckoutFragment.kt
+++ app/src/main/java/com/example/gearswap/ui/home/checkout/CheckoutFragment.kt
@@ -1,13 +1,21 @@
 package com.example.gearswap.ui.home.checkout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.gearswap.R
 import com.example.gearswap.databinding.FragmentCheckoutBinding
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,34 +28,99 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CheckoutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var paymentSheet: PaymentSheet
+    private var paymentIntentClientSecret: String? = null
 
     private var _binding: FragmentCheckoutBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkout, container, false)
+        _binding = FragmentCheckoutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentCheckoutBinding.bind(view)
-        binding.button.setOnClickListener{findNavController().navigate(R.id.show_confirmation)}
 
+
+
+            paymentSheet = PaymentSheet(this) { result ->
+                onPaymentSheetResult(result)
+            }
+
+
+        setupPaymentConfiguration()
+//        simulateFetchPaymentIntent()
+fetchPaymentIntentClientSecret()
+
+        binding.button.setOnClickListener {
+            paymentIntentClientSecret?.let { secret ->
+                presentPaymentSheet(secret)
+            }
+        }
+    }
+
+    private fun setupPaymentConfiguration() {
+        PaymentConfiguration.init(requireContext(), "pk_test_TYooMQauvdEDq54NiTphI7jx")
+    }
+
+    private fun simulateFetchPaymentIntent() {
+        // This is where you'd fetch from your server in a real scenario
+        // Simulating a fetch with a hardcoded client secret from a test PaymentIntent
+        paymentIntentClientSecret = "pi_3PA6oi2eZvKYlo2C1eXf5Iil_secret_3fzEz7fl6PkEq7TGqYHnCihxq"
+
+    }
+
+    private fun fetchPaymentIntentClientSecret() {
+        // This is a placeholder URL for demonstration purposes only.
+        // Replace with the URL of your server where you're creating the PaymentIntent.
+        //TODO: Replace with your own server URL
+        val url = "https://tidy-rectangular-coat.glitch.me/checkout"
+
+        val request = JsonObjectRequest(
+            Request.Method.POST, url, null,
+            { response ->
+                // The response from the server includes the client secret
+                paymentIntentClientSecret = response.getString("paymentIntent")
+            },
+            { error ->
+                // Handle the error
+                Log.d("PaymentSheet" ,"Error: ${error.message}")
+                Toast.makeText(requireContext(), "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        // Add the request to the RequestQueue.
+        Volley.newRequestQueue(requireContext()).add(request)
+    }
+
+
+    private fun presentPaymentSheet(clientSecret: String) {
+        val configuration = PaymentSheet.Configuration(
+            merchantDisplayName = "Demo Store"
+            // Add additional configuration as needed
+        )
+        paymentSheet.presentWithPaymentIntent(clientSecret, configuration)
+    }
+
+    private fun onPaymentSheetResult(result: PaymentSheetResult) {
+        when (result) {
+            is PaymentSheetResult.Completed -> {
+                Log.d("PaymentSheet", "Payment completed successfully.")
+                findNavController().navigate(R.id.show_confirmation)
+            }
+            is PaymentSheetResult.Canceled -> {
+                Log.d("PaymentSheet", "Payment was canceled.")
+                Toast.makeText(requireContext(), "Payment was canceled.", Toast.LENGTH_SHORT).show()
+            }
+            is PaymentSheetResult.Failed -> {
+                Log.e("PaymentSheet", "Payment failed with exception: ${result.error?.message}")
+                Toast.makeText(requireContext(), "Payment failed with exception: ${result.error?.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     companion object {
